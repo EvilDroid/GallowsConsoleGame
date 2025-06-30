f55d85d5da0bfd817bbd6dbb447f9ec94d9c9ba9
EvilDroid <aikelon@gmail.com>
2025-07-01 02:25:59 +0400

    Add validation

--- src/main/java/LetterInputByUser.java
+++ src/main/java/LetterInputByUser.java
@@ -1,25 +1,59 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class LetterInputByUser {
+
+
     public static String getInput(){
         Scanner inputFlow = new Scanner(System.in);
         String input = "";
         do {
             input = inputFlow.next().toLowerCase();
-
-            if(input.equals("exit")){
-                //initiate exiting from game
-                break;
-            }
-            if(input.length() > 1){
-                System.out.println("Введите только 1 букву.");
+            if(validateLetter(input) == false){
+                System.out.println("Пожалуйста введите букву русского языка. Или exit, если хотите выйти.");
                 continue;
             }
+
+            if(input.equals("exit")){
+                System.exit(0);
+                break;
+            }
+
             break;
         }
         while (true);
         //inputFlow.close();
         return input;
+    }
+
+    public static boolean validateLetter (String text){
+
+        List<String> KEYWORDS = Arrays.asList("exit");
+
+        if (text.equals("") || text == null){
+            return false;
+        }
+
+        if (text.length() == 1){
+            return isCyrillicLetter(text.charAt(0));
+        } else {
+            return KEYWORDS.contains(text);
+        }
+    }
+
+    public static boolean isCyrillicLetter (char c){
+        char [] validLetters = {
+                'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й',
+                'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
+                'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'
+        };
+        for (char letter :  validLetters){
+            if(c == letter){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
